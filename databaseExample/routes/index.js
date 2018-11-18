3d97f762e25b35bfd148345aec2bc2bb5964bdc7
fieldanimal <mallocation@naver.com>
2018-11-19 04:12:14 +0900

    회원가입기능 추가

--- databaseExample/routes/index.js
+++ databaseExample/routes/index.js
@@ -21,7 +21,28 @@ MongoClient.connect(url, function (err, client) {
 router.get('/', function (req, res, next) {
   res.render('login');
 });
+router.get('/addUser',function(req,res){
+  res.render('addUser');
+});
+var addUser = function (database, id, password,name, callback) {
+  console.log('addUser 호출됨'+id+' '+password+' '+name);
+  var users = database.collection('users');
+  users.insertMany([{ "id": id, "password": password,"name":name }],function (err, result) {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+    console.log('추가된개체수:'+result.insertedCnt)
+    if (result) {
+      console.log('사용자 레코드 추가됨');
+    }
+    else {
+      console.log('추가실패');
+    }
 
+    callback(null,result);
+  });
+}
 var authUser = function (database, id, password, callback) {
   console.log('authUser 호출됨');
   console.dir(database);
@@ -39,11 +60,33 @@ var authUser = function (database, id, password, callback) {
     else {
       console.log('못찾음');
       callback(null, null);
-
+      
     }
   });
 }
-
+router.post('/process/addUser',function(req,res){
+  var id=req.body.id;
+  var password=req.body.password;
+  var name=req.body.name;
+  if(db){
+    addUser(db,id,password,name,function(err,result){
+      if(err)throw err;
+      if(result){
+        console.dir(result);
+        console.log('회원가입 성공');
+        res.render('login');
+      }
+      else{
+        console.log('회원가입 실패');
+        res.render('login');
+      }
+    });
+  }
+  else{
+    res.write('<h1>databaseConnectionError</h1>');
+  }
+  
+});
 router.post('/process/login', function (req, res) {
   var id = req.body.id;
   var password = req.body.password;
@@ -55,7 +98,13 @@ router.post('/process/login', function (req, res) {
         var username = docs[0].name;
         res.render('success', { id: id, password: password });
       }
+      else{
+        res.write('<h1>please check your id & password</h1>');
+      }
     })
+  }
+  else{
+    res.write('<h1>databaseConnectionError</h1>');
   }
 });
 
